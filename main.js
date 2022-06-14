dfbf1df45b473c244521e416478822b4d752ccc3
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-06-15 07:52:40 +0900

    List Rendering

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ var app = new Vue({
   data: {
     product: 'Socks',
     image: './assets/vmSocks-green.jpg',
-    inStock: false
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutual"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ],
+    sizes: [
+      {
+        sizeId: 2234,
+        size: "23cm"
+      },
+      {
+        sizeId: 2234,
+        size: "25cm"
+      }
+    ]
   }
 })
